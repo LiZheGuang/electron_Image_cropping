e1b551b2b1737903454155b4a6573ce445dec76f
LiZheGuang <1030660726@qq.com>
2023-05-31 21:03:15 +0800

    feat: 🎸 图标设置

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 
+const { shell } = require('electron')
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
