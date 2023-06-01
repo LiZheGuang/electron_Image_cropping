7b52057d29d089fcf8cc8c4a1df333610b5a0a01
LiZheGuang <1030660726@qq.com>
2023-06-01 19:13:33 +0800

    feat: 🎸 部署发布
    ✅ Closes: g

--- src/main.js
+++ src/main.js
@@ -101,7 +101,9 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.webContents.openDevTools();
+  }
 };
 
 // This method will be called when Electron has finished
