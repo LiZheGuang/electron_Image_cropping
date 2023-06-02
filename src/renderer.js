6323a5310cbceff0b499e7ae2f8c6822286f16a3
LiZheGuang <1030660726@qq.com>
2023-06-02 17:19:46 +0800

    feat: 🎸 code push

--- src/renderer.js
+++ src/renderer.js
@@ -10,15 +10,7 @@ function cropperInit() {
   cropper = new Cropper(image, {
     aspectRatio: 1 / 1,
     zoomOnWheel:false,
-    crop(event) {
-      // console.log(event.detail.x);
-      // console.log(event.detail.y);
-      // console.log(event.detail.width);
-      // console.log(event.detail.height);
-      // console.log(event.detail.rotate);
-      // console.log(event.detail.scaleX);
-      // console.log(event.detail.scaleY);
-    },
+  
   });
 }
 
